1b7add72966b815270a61cdf8e422524fb4e18e9
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2022-09-03 14:08:31 +0300

    feat: connect to database

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import db from './db.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/categories', async (req, res) => {
+  const categories = await db.query('SELECT * FROM categories');
+  res.json(categories.rows);
 });
 
 app.listen(port, () => {
