4d2083c91f7eb940f014b76a979f68c935ea516b
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2022-10-07 20:52:44 +0300

    feat: export app for vercel, format index.js

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import categoryRouter from './src/routes/category.route.js'
-import cardRouter from './src/routes/card.route.js'
-import cors from 'cors'
+import cors from 'cors';
+import categoryRouter from './src/routes/category.route.js';
+import cardRouter from './src/routes/card.route.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json({ limit: '50mb', extended: true }));
-app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
-app.use('/api', [categoryRouter, cardRouter])
+app.use(
+  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }),
+);
+app.use('/api', [categoryRouter, cardRouter]);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+export default app;
